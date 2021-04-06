abb06d1e2971c0f4d2f081de61a298ed448a1356
dnjs0718 <mystund@naver.com>
2021-04-06 16:50:07 +0900

    210406

--- hi.py
+++ hi.py
@@ -1,3 +1,14 @@
-import sys
-arr = [int(sys.stdin.readline()) for i in range(int(sys.stdin.readline()))]
-print(max(arr))
+def get_len_of_str(s):
+    # 아래 코드를 작성해주세요.
+    str = ''
+    arr = []
+    for i in range(len(s)):
+      if s[i] not in str:
+        str += s[i]
+      else:
+        arr.append(str)
+        str =''
+        str += s[i]
+    arr.append(str)
+    return max(len(m) for m in arr)
+print(get_len_of_str('abc decabfghij'))
