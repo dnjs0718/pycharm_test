e075d5d430b12116533f6829269e2d758331a6c4
dnjs0718 <mystund@naver.com>
2021-04-25 23:13:57 +0900

    파이썬 테스트

--- noop.py
+++ noop.py
@@ -1,23 +1,6 @@
-def roman_to_num(s):
-    symbol = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    result = 0
-    for i in range(len(s)):
-        if symbol[s[i]] == 5 or symbol[s[i]] == 10:
-            if symbol[s[i - 1]] == 1:
-                result += symbol[s[i]] - 2 * symbol[s[i - 1]]
+a = int(input())
 
-
-        if symbol[s[i]] == 50 or symbol[s[i]] == 100:
-            if symbol[s[i - 1]] == 10:
-                result += symbol[s[i]] - 2 * symbol[s[i - 1]]
-
-        if symbol[s[i]] == 500 or symbol[s[i]] == 1000:
-            if symbol[s[i - 1]] == 100:
-                result += symbol[s[i]] - 2 * symbol[s[i - 1]]
-
-        else:
-            result += symbol[s[i]]
-
-    return result
-
-print(roman_to_num('MCMXCIV'))
+if not (a ==3 or 5 or 6 or 7):
+    print('nono')
+else:
+    print('yes')
